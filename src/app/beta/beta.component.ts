88a10834196784294ef6bf4676990ebfdc6a062a
Masnun-13 <masnun111112@gmail.com>
2023-05-17 15:46:28 +0600

    alpha, beta and delta have functionality, app mostly stable

--- src/app/beta/beta.component.ts
+++ src/app/beta/beta.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-beta',
   templateUrl: "./beta.component.html",
   styleUrls: ['./beta.component.css']
 })
 export class BetaComponent {
+  val1 : number = 0;
+  val2 : number = 1;
+  res : number = 0;
 
+  add(){
+    let x = Number(this.val1)
+    let y = Number(this.val2)
+    let z = Number(x+y)
+    this.res=z
+  }
+
+  sub(){
+    let x = Number(this.val1)
+    let y = Number(this.val2)
+    let z = Number(x-y)
+    this.res=z
+  }
+
+  mul(){
+    let x = Number(this.val1)
+    let y = Number(this.val2)
+    let z = Number(x*y)
+    this.res=z
+  }
+
+
+  div(){
+    let x = Number(this.val1)
+    let y = Number(this.val2)
+    let z = Number(x/y)
+    this.res=z
+  }
+
+  mod(){
+    let x = Number(this.val1)
+    let y = Number(this.val2)
+    let z = Number(x%y)
+    this.res=z
+  }
+
+  pow(){
+    let x = Number(this.val1)
+    let y = Number(this.val2)
+    let z = Number(x**y)
+    this.res=z
+  }
 }
